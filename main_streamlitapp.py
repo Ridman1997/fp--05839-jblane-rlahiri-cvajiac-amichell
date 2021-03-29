812b0a31d8019015c6bb62c7d133b673d5f28c7f
jtblane <63323583+jtblane@users.noreply.github.com>
2021-03-29 11:23:45 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -1,7 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 
+import streamlit as st
+import pandas as pd
+import altair as alt
+from datetime import date
+
+st.title("Towards a Better Spark UI")
+st.text("An Interactive Visualization")
+
+
+st.markdown("_**Welcome!**_" )
+st.markdown(" Stuff...and more stuff.")
