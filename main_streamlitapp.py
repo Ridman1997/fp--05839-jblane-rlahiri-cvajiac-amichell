0c0a959307e3082c1b14fe1c7262c6a2413c7cc6
jtblane <63323583+jtblane@users.noreply.github.com>
2021-03-31 15:03:13 -0400

    Update main_streamlitapp.py

--- main_streamlitapp.py
+++ main_streamlitapp.py
@@ -5,19 +5,9 @@ import pandas as pd
 import altair as alt
 from datetime import date
 
-st.title("Towards a Better Spark UI")
+st.title("⭐Towards a Better Spark UI⭐")
 st.text("An Interactive Visualization")
 
 
 st.markdown("_**Welcome!**_" )
 st.markdown(" Stuff...and more stuff.")
-st.markdown(
-    """
-    <style>
-    .reportview-container {
-        background: url("background01.jpg")
-    }
-    </style>
-    """,
-    unsafe_allow_html=True
-)
